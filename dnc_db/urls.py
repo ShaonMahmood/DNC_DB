d2e311781bf7b9c9cbb05a09cea46cd751197468
Shaon1342 <mahmood_shaon@yahoo.com>
2018-03-29 12:48:44 +0600

    debug=false

--- dnc_db/urls.py
+++ dnc_db/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.models import User
 
-from dnc_db import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
