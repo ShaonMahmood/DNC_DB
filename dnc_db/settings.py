c96206549877af3beac5ab1e14c91d654371b7fa
Shaon1342 <mahmood_shaon@yahoo.com>
2018-03-29 12:58:14 +0600

    debug=false

--- dnc_db/settings.py
+++ dnc_db/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '-(%jpo&vbk2+vjz_$^#o^moxuuotazo1t349x$i)ip_@ic76g_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = os.environ.get('DOKKU_DEBUG',True)
 
 
 ALLOWED_HOSTS = ['*']
